8f22924c4b5963c45e7637b903fa90a56286cafb
Mahmoud-A-Noor <mahmoudnoor917@gmail.com>
2025-04-19 14:16:23 +0200

    removed unused import

--- frontend/src/pages/VoiceChat.tsx
+++ frontend/src/pages/VoiceChat.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import RecordRTC from 'recordrtc';
 import axios from 'axios';
 import { getOrCreateChatId } from "@/services/session";
-import VoiceButton from "@components/chat/VoiceButton";
+
 
 const VoiceChat = () => {
   const chatId = getOrCreateChatId();
